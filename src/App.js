8d44c8bd8de857dce22c3cec71a7b40ce42041a1
Mesut TALEBİ <mesut.talebi@acerpro.com.tr>
2019-09-04 07:54:34 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi There, I'm learning docker. My Name is Mesut.
         </p>
         <a
           className="App-link"
