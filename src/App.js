c2360e59715dce65c974e03165fb2eaff818356f
Mesut TALEBİ <mesut.talebi@acerpro.com.tr>
2019-09-04 15:54:05 +0300

    changed app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There, I'm learning docker. My Name is Mesut.
+          Hi There, I'm learning docker. I changed in feature branch.
         </p>
         <a
           className="App-link"
